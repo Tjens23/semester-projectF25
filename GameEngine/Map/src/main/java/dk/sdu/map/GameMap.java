55a7a0bcd647e73b78e421962c3b74fb1201f9b8
tjens23 <tjens23@student.sdu.dk>
2025-04-04 13:33:42 +0200

    Map Implemented\nAuthor: Wesam

---
+++ GameEngine/Map/src/main/java/dk/sdu/map/GameMap.java
@@ -0,0 +1,53 @@
+package dk.sdu.map;
+
+
+import dk.sdu.player.entity.Entity;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.ArrayList;
+import java.util.List;
+public class GameMap implements IMapService {
+g
+    private final Map<String, Entity> entityMap = new ConcurrentHashMap<>();
+
+    @Override
+    public String addEntity(Entity entity) {
+        entityMap.put(entity.getID(), entity);
+        return entity.getID();
+    }
+
+    @Override
+    public void removeEntity(String entityID) {
+        entityMap.remove(entityID);
+    }
+
+    @Override
+    public void removeEntity(Entity entity) {
+        entityMap.remove(entity.getID());
+    }
+
+    @Override
+    public Collection<Entity> getEntities() {
+        return entityMap.values();
+    }
+
+    @Override
+    public <E extends Entity> List<Entity> getEntities(Class<E>... entityTypes) {
+        List<Entity> result = new ArrayList<>();
+        for (Entity e : getEntities()) {
+            for (Class<E> entityType : entityTypes) {
+                if (entityType.equals(e.getClass())) {
+                    result.add(e);
+                }
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Entity getEntity(String ID) {
+        return entityMap.get(ID);
+    }
+}
