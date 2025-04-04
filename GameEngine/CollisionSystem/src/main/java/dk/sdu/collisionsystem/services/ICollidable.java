4588ef777c55481d34549ffaee666fefe4d8e382
tubnielsen <tubnielsen@gmail.com>
2025-04-04 12:26:54 +0200

    new interface ICollidable

---
+++ GameEngine/CollisionSystem/src/main/java/dk/sdu/collisionsystem/services/ICollidable.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dk.sdu.collisionsystem.services;
+
+import dk.sdu.map.World;
+import dk.sdu.entity.Entity;
+
+
+/**
+ *
+ * @author tubnielsen
+ */
+public interface ICollidable {
+    
+    void process(GameData gameData, World, world);
+    
+    boolean collision(Entity e1, Entity e2);
+   
+}
