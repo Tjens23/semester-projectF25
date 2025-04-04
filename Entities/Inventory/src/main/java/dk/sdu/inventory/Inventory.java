84821d88c8805b2ae68c74fce75cd4c46f8b734d
tjens23 <tjens23@student.sdu.dk>
2025-04-04 11:58:34 +0200

    INventory and item

---
+++ Entities/Inventory/src/main/java/dk/sdu/inventory/Inventory.java
@@ -0,0 +1,51 @@
+package dk.sdu.inventory;
+
+import java.util.ArrayList;
+
+public class Inventory {
+    public ArrayList<Item> inventory = new ArrayList<>();
+
+    public void AddItemToInventory(Item item) {
+        int inventorySlots = 8;
+        if (inventory.size() >= inventorySlots) {
+            return;
+        }
+        inventory.add(item);
+
+    }
+
+    public void RemoveItemFromInventory(int itemIndex) {
+        for (int i = 0; i < inventory.size(); i++) {
+            if (inventory.get(i).index == itemIndex)
+                inventory.remove(itemIndex);
+        }
+    }
+
+
+    public Item CheckForItem(int itemIndex) {
+        for (Item item :
+                inventory) {
+            if (item.index == itemIndex)
+                return item;
+        }
+        return null;
+    }
+
+    public Item CheckForItem(String itemName) {
+        for (Item item :
+                inventory) {
+            if (item.name.equalsIgnoreCase(itemName))
+                return item;
+        }
+        return null;
+    }
+
+    public Boolean BoolCheckForItemType(Item.itemTypes itemTypes) {
+        for (Item item :
+                inventory) {
+            if (item.itemType.equals(itemTypes))
+                return true;
+        }
+        return false;
+    }
+}
---
+++ Entities/Player/src/main/java/dk/sdu/player/Player.java
@@ -0,0 +1,4 @@
+package dk.sdu.player;
+
+public class Player {
+}
